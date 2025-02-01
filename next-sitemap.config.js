c9be13cdd9a0e731573ad99bbbaf08b4a491fd3b
Abhishek <abhi120730@gmail.com>
2025-02-01 18:16:57 +0530

    commeted the new file which is next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://abhishekworks.com', // Replace with your domain
+    generateRobotsTxt: true, // Generate a robots.txt file (optional)
+  };
+  
